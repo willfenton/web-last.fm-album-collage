dc1ef917edbd73a3e85be4f53f0b20dce0e3fd8a
willfenton <wfenton@ualberta.ca>
2020-04-23 03:07:11 -0600

    show top track for each album

--- lastfm/get_top_albums_json.py
+++ lastfm/get_top_albums_json.py
@@ -8,9 +8,9 @@ def main():
     }
 
     db = sqlite3.connect("database.sqlite3")
-    results = db.execute("SELECT album_name, artist_name, extralarge_image_url, MIN(unix_timestamp), MAX(unix_timestamp), COUNT(*) FROM scrobbles GROUP BY album_name, artist_name HAVING COUNT(DISTINCT track_mbid) > 1 ORDER BY COUNT(*) DESC LIMIT 200;").fetchall()
-    for result in results:
-        album_name, artist_name, image_url, min_timestamp, max_timestamp, num_scrobbles = result
+    top_albums = db.execute("SELECT album_name, artist_name, extralarge_image_url, MIN(unix_timestamp), MAX(unix_timestamp), COUNT(*) FROM scrobbles GROUP BY album_name, artist_name HAVING COUNT(DISTINCT track_mbid) > 1 ORDER BY COUNT(*) DESC LIMIT 200;").fetchall()
+    for top_album in top_albums:
+        album_name, artist_name, image_url, min_timestamp, max_timestamp, num_scrobbles = top_album
         print(album_name, artist_name, num_scrobbles)
         album = {
             "album_name": album_name,
@@ -20,6 +20,10 @@ def main():
             "max_timestamp": max_timestamp,
             "num_scrobbles": num_scrobbles
         }
+
+        top_tracks = db.execute("SELECT track_name, COUNT(*) FROM scrobbles WHERE album_name=? AND artist_name=? GROUP BY track_name ORDER BY COUNT(*) DESC;", [album_name, artist_name]).fetchall()
+        album["top_track"] = top_tracks[0][0]
+        
         albums["albums"].append(album)
     
     with open("albums.json", "w") as f:
